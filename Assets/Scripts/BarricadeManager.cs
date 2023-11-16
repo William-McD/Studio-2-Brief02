0b18497a447fe68fbbb762f2b9a18d25fc718366
Shadow43 <ShadowFox_43@hotmail.com>
2023-11-16 17:48:15 +1000

    Barricades V2 - Nathan
    partially working, but not fully. Don't know why. I have a feeling it's how the enemy finds the nearest barricade

--- Assets/Scripts/BarricadeManager.cs
+++ Assets/Scripts/BarricadeManager.cs
@@ -6,6 +6,20 @@ using UnityEngine;
 
 public class BarricadeManager : MonoBehaviour
 {
+
+    //Nathan
+    public GameObject prefabA;
+    public GameObject prefabB;
+    public GameObject prefabC;
+    public GameObject prefabD;
+    public GameObject prefabE;
+    public bool ShieldedBarricade;
+    public bool SlightlyDamaged;
+    public bool HalfDamaged;
+    public bool MostlyDamaged;
+    public bool BarricadeDestroyed;
+    public int barricadeHP;
+    //Nathan
     public int barricadeHealth;
     public TMP_Text barricadeText;
     GameObject[] barricades;
@@ -18,11 +32,16 @@ public class BarricadeManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
-    }
+        //Nathan
+        GameObject clone = Instantiate(prefabA, transform.position, transform.rotation);
+        clone.GetComponent<Barricade>().script = this;
+        barricadeHP = 1;
+        //Nathan
 
-    // Update is called once per frame
-    void Update()
+}
+
+// Update is called once per frame
+void Update()
     {
         barricadeText.text = (barricadeHealth + "%");
 
@@ -37,6 +56,39 @@ public class BarricadeManager : MonoBehaviour
                 b.SetActive(true);
             }    
         }
+
+        //Nathan
+        if ((ShieldedBarricade == false) && (barricadeHP == 2))
+        {
+            GameObject clone = Instantiate(prefabB, transform.position, transform.rotation);
+            clone.GetComponent<Barricade>().script = this;
+            ShieldedBarricade = true;
+        }
+        if ((SlightlyDamaged == false) && (barricadeHP == 3))
+        {
+            GameObject clone = Instantiate(prefabB, transform.position, transform.rotation);
+            clone.GetComponent<Barricade>().script = this;
+            SlightlyDamaged = true;
+        }
+        if ((HalfDamaged == false) && (barricadeHP == 4))
+        {
+            GameObject clone = Instantiate(prefabB, transform.position, transform.rotation);
+            clone.GetComponent<Barricade>().script = this;
+            HalfDamaged = true;
+        }
+        if ((MostlyDamaged == false) && (barricadeHP == 5))
+        {
+            GameObject clone = Instantiate(prefabB, transform.position, transform.rotation);
+            clone.GetComponent<Barricade>().script = this;
+            MostlyDamaged = true;
+        }
+        if ((BarricadeDestroyed == false) && (barricadeHP == 6))
+        {
+            GameObject clone = Instantiate(prefabB, transform.position, transform.rotation);
+            clone.GetComponent<Barricade>().script = this;
+            BarricadeDestroyed = true;
+        }
+        //nathan
     }
     void DestroyBarricade()
     {
